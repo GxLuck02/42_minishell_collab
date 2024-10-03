10c5441aaa3ae8efdbe3be6c9b753f97f8f33a03
tmontani25 <teomontani27@gmail.com>
2024-10-03 15:57:35 +0200

    pipe_test

--- Srcs/Exec/pipe_utils.c
+++ Srcs/Exec/pipe_utils.c
@@ -6,10 +6,11 @@ si il y a une cmd prev
 dup2(pipe_fd)
 */
 
-void	execute_pipe(t_data *data, int pipe_fd[]);
+void	execute_pipe(t_data *data, int pipe_fd[])
 {
 	pid_t	pid;
 	int		status;
+
 	pid = fork();
 	if (pid < 0)
 	{
@@ -18,13 +19,18 @@ void	execute_pipe(t_data *data, int pipe_fd[]);
 	}
 	else if (pid == 0)
 	{
-		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[0]);
+		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
 		make_cmd(data);
 	}
 	else
+	{
 		waitpid(pid, &status, 0);
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], STDIN_FILENO);
+		close(pipe_fd[0]);
+	}
 }
 
 void	handle_pipe(t_data *data)
