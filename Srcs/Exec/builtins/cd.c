dcb897feeee1472beb401bacb5d0fa293e3410ae
tmontani25 <teomontani27@gmail.com>
2024-10-09 08:53:34 +0200

    cd ok mais sans old_pwd

--- Srcs/Exec/builtins/cd.c
+++ Srcs/Exec/builtins/cd.c
@@ -1,20 +1,31 @@
 #include "../../../Includes/minishell.h"
+
+int	update_old_pwd(t_data *data, char *old_pwd)
+{
+	t_env *OLD_PWD;
+
+	OLD_PWD = ft_getenv("OLD_PWD", data->env);
+	if (OLD_PWD)
+	{
+		OLD_PWD->value = old_pwd;
+		return (1);
+	}
+	return (0);
+}
 int	cd_no_args(t_data *data)
 {
 	t_env	*PWD;
-	t_env	*OLD_PWD;
-	char	*old_pwd;
 	t_env	*HOME;
+	char	*old_pwd;
 
 	HOME = ft_getenv("HOME", data->env);
+	old_pwd = getcwd(NULL, 0);
 	if (!chdir(HOME->value))
 	{
 		PWD = ft_getenv("PWD", data->env);
-		old_pwd = PWD->value;
 		free(PWD->value);
 		PWD->value = ft_strdup(getcwd(NULL, 0));
-		OLD_PWD = ft_getenv("OLD_PWD", data->env);
-		OLD_PWD->value = old_pwd;
+		update_old_pwd(data, old_pwd);
 		return (1);
 	}
 	else
@@ -22,9 +33,7 @@ int	cd_no_args(t_data *data)
 		printf("bash %s: No such file or directory\n", data->cmd->cmd_param[0]);
 		return (0);
 	}
-	return (1);
 }
-
 /*
 1. verifier si le dossier est valide
 2. mettre a jour la variable env pwd
@@ -35,12 +44,10 @@ int	cd_no_args(t_data *data)
 int ft_cd(t_data *data)
 {
 	t_env	*PWD;
-	t_env	*OLD_PWD;
 	char	*old_pwd;
 
 	if(!data->cmd->cmd_param[1])
 	{
-		puts("cd no args\n");
 		if(!cd_no_args(data))
 			return (0);
 		else
@@ -48,14 +55,13 @@ int ft_cd(t_data *data)
 	}
 	else
 	{
+		old_pwd = getcwd(NULL, 0);
 		if (!chdir(data->cmd->cmd_param[1]))
 		{
 			PWD = ft_getenv("PWD", data->env);
-			old_pwd = PWD->value;
 			free(PWD->value);
 			PWD->value = ft_strdup(getcwd(NULL, 0));
-			OLD_PWD = ft_getenv("OLD_PWD", data->env);
-			OLD_PWD->value = old_pwd;
+			update_old_pwd(data, old_pwd);
 			return (1);
 		}
 		else
@@ -64,5 +70,4 @@ int ft_cd(t_data *data)
 			return (0);
 		}
 	}
-	return (1);
 }
