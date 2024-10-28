7d7d6681dc2cfe9cd2d1f3a2983e972ee9eadd7d
ttreichl <ttreichl@student.42lausanne.ch>
2024-10-28 17:38:30 +0100

    mieux

--- Srcs/Exec/utils4.c
+++ Srcs/Exec/utils4.c
@@ -6,7 +6,7 @@
 /*   By: ttreichl <ttreichl@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 16:07:13 by tmontani          #+#    #+#             */
-/*   Updated: 2024/10/28 17:32:20 by ttreichl         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:38:23 by ttreichl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,6 @@ void	wait_all(t_data *data, int len_cmd)
 		waitpid(data->pid_tab[i], &status, 0);
 		if (WIFEXITED(status))
 			data->exit_code = WEXITSTATUS(status);
-		else
-			printf("Child process error\n");
 		i++;
 	}
 }
